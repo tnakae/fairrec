62c4d946877796c5b259d2c67d28a11238170b8f
Toshihiro NAKAE <toshihiro.nakae@bizreach.co.jp>
2020-05-08 10:32:12 +0900

    Modified

--- fairrec/fairrec.py
+++ fairrec/fairrec.py
@@ -1,63 +1,75 @@
 """Fair-Rec"""
 import numpy as np
 import scipy.sparse as sps
-from sklearn.metrics import pairwise_distances
-
-
-def pairwise_distances_argsort(X, Y, metrics='euclidian'):
-    distances = pairwise_distances(X, Y, metrics)
-    topk_pos = np.argsort(distances, axis=1)[:, ::-1]
-    return ranking
+from sklearn.metrics import pairwise_distances_argmin
 
 
 class FairRec:
     def __init__(self, user_embeddings, item_embeddings,
-                 k=10, alpha=1.0, metrics="cosine"):
+                 k=10, alpha=1.0, metric="cosine"):
         self.user_emb = user_embeddings
         self.item_emb = item_embeddings
-        self.k = k
-        self.alpha = alpha
-        self.metrics = metrics
-
-        # num of user, item
         self.num_user = self.user_emb.shape[0]
         self.num_item = self.item_emb.shape[0]
 
-        # lower bound of assignment for each item
-        self.E_min = np.floor(self.M * k / self.N * alpha)
+        self.k = k
+        self.alpha = alpha
+        self.metric = metric
+
+        self.assignment = None
+        self.sigma = None
+        self.stocks = None
+        self.sigma_idx = 0
+
+        self.min_assigned = int(np.floor(
+            self.num_user * k / self.num_item * self.alpha))
 
     def _greedy_round_robin(self):
-        """Assign items to users based on *greedy-round-robin* rule.
+        """Assign items to users based on greedy-round-robin rule."""
+        while True:
+            # 在庫がない場合は終了
+            if self.stocks.sum() == 0:
+                break
 
-        Parameters:
-        -----------
-        """
-        for i in range(self.num_user):
-            user_idx = self.sigma[i]
-            target_user_emb = self.user_emb[user_idx][None,:]
-            distance = pairwise_distances(self.item_emb, target_user_emb)
-            ranking = np.argsort(distance)[::-1]
+            # 対象ユーザ
+            user_idx = self.sigma[self.sigma_idx]
 
-            for item_idx in ranking:
-                
+            # このユーザがすでに最大数を取得している場合は終了
+            if self.assignment[user_idx].nnz >= self.k:
+                break
 
-        pass
+            # このユーザが持っていない在庫のあるアイテムを持ってくる
+            assigned_items = self.assignment[user_idx].nonzero()[1]
+            valid_items = np.argwhere(self.stocks > 0).squeeze()
+            valid_items = valid_items[~np.isin(valid_items, assigned_items)]
 
-    def _prepare_phase1(self):
-        # 各アイテムの残個数
-        self.stocks = np.array([self.E_min] * self.num_item)
-        # ユーザの割り当て順
-        self.sigma = np.random.choice(M, size=M, replace=False)
+            # このユーザが取れるアイテム在庫がない場合は終了
+            if len(valid_items) == 0:
+                break
 
-    def _prepare_phase2(self):
-        # 各アイテムの残個数は最大割り当てとする
-        self.stocks = np.array([self.k] * self.num_item)
-        # ユーザの割り当て順
-        self.sigma = np.random.choice(M, size=M, replace=False)
+            # 一番距離が近いアイテムを探す
+            target_user_emb = self.user_emb[user_idx][None, :]
+            valid_items_emb = self.item_emb[valid_items]
+            nearest_idx = pairwise_distances_argmin(
+                target_user_emb, valid_items_emb,
+                metric=self.metric)[0]
+            nearest_item = valid_items[nearest_idx]
+
+            # アイテムを割り当て
+            self.assignment[user_idx, nearest_item] += 1
+            self.stocks[nearest_item] -= 1
+
+            self.sigma_idx = (self.sigma_idx + 1) % self.num_user
 
     def recommend(self):
-        self.X = sps.csr_matrix(shape=[self.num_user, self.num_item])
-        self._prepare_phase1()
+        self.sigma = np.random.choice(
+            self.num_user, size=self.num_user, replace=False)
+        self.assignment = sps.lil_matrix(
+            (self.num_user, self.num_item), dtype=np.uint8)
+
+        self.stocks = np.repeat(self.min_assigned, self.num_item)
         self._greedy_round_robin()
-        self._prepare_phase2()
+        self.stocks = np.repeat(self.num_user, self.num_item)
         self._greedy_round_robin()
+
+        return self.assignment
